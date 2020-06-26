d8d71ac4e9873a321daaa93f7e4738e2abd81bfb
Chris Ferguson <cfergus7@gmail.com>
2020-06-25 23:29:52 -0500

    connectionJS done orm structord

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Set up My SQL connection
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+});
+
+// Establish connection
+connection.connect(function(err){
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for ORM
+module.exports = connection;
