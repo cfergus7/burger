6c8e63078a1cd60ac1afab053a02d69e625b8c31
Chris Ferguson <cfergus7@gmail.com>
2020-06-30 10:54:19 -0500

    fixed conncection issue

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,10 @@
 // Set up My SQL connection
 var mysql = require("mysql");
 
-var connection;
-
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
+    var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
